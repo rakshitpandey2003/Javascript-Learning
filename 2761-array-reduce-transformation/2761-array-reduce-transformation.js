f900d8f295ee0eeda2538e4dc611a829055c9eab
rakshitpandey2003 <110710174+rakshitpandey2003@users.noreply.github.com>
2023-08-29 18:25:10 +0530

    Time: 61 ms (36.01%), Space: 42.5 MB (33.35%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+    nums.forEach(i=>{val = fn(val , i)});
+    return val;
+};
