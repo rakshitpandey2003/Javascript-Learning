d6413e7ce2d3be3e4da670ee8435fa7f537652d0
rakshitpandey2003 <110710174+rakshitpandey2003@users.noreply.github.com>
2023-09-09 19:31:05 +0530

    Time: 51 ms (69.19%), Space: 42.1 MB (34.62%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let rem = 1;
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i] == 9){
+            digits[i] = 0;
+            if(i==0) digits.unshift(1);
+        }
+        else {digits[i] += 1; break;}
+    }
+    return digits;
+};
